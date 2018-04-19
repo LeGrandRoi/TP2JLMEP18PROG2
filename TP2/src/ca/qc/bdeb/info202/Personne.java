e9d9d6e3ab1ae889d72e107aaf7cca5f4412ef3a
LeGrandRoi <38329401+LeGrandRoi@users.noreply.github.com>
2018-04-19 14:39:22 -0400

    ListeFichier
    Liste du fichier de personnes.txt

--- TP2/src/ca/qc/bdeb/info202/Personne.java
+++ TP2/src/ca/qc/bdeb/info202/Personne.java
@@ -5,12 +5,49 @@
  */
 package ca.qc.bdeb.info202;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 1660802
  */
 public class Personne {
-    
+    ArrayList<String> liste = new ArrayList();
     private String nom, nomUtilisisateur, codeEncrypte;
     private int age;
+
+    public Personne(String nom, String nomUtilisisateur, String codeEncrypte, int age) {
+        this.nom = nom;
+        this.nomUtilisisateur = nomUtilisisateur;
+        this.codeEncrypte = codeEncrypte;
+        this.age = age;
+    }
+    
+    public void lecturePersonne(){
+        BufferedReader lecture=null;
+        String ligne=null;
+        
+        while(ligne!=null){
+        try{
+             lecture = new BufferedReader ( new FileReader("personnes.txt"));
+        }catch(FileNotFoundException e){ 
+            System.out.println("Le fichier n'est pas trouvé.");
+        }
+        try {   
+           ligne=lecture.readLine();
+        } catch (IOException ex) {
+            
+        }
+        
+        liste.add(ligne);
+        
+    }
+    }
+    
 }
