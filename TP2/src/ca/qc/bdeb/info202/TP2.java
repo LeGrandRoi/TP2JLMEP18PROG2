85ae13335bc2e80619fc7d8187a2c18209071fcd
LeGrandRoi <38329401+LeGrandRoi@users.noreply.github.com>
2018-04-19 13:11:53 -0400

    Code

---
+++ TP2/src/ca/qc/bdeb/info202/TP2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * and open the template in the editor.
+ */
+package ca.qc.bdeb.info202;
+
+/**
+ *
+ * @author 1660802
+ */
+public class TP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+        System.out.println("Allo");
+        
+        System.out.println("Lol");
+        
+        System.out.println("salut");
+        String allo ="allo";
+        
+        System.out.println("lel");
+    }
+    
+}
